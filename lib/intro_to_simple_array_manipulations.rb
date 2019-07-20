2bf3764f45619f3c77fbef8938c603dcedd92ed0
Andrew Williams <1williamsaj@gmail.com>
2019-07-20 21:16:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ end
 
 def using_pop(array)
   array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  array.pop
+  removed_continent = array.pop
 end
 
 def pop_with_args(array)
