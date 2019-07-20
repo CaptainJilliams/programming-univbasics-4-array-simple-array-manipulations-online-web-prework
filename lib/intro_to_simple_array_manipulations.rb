19fa3e893e1b81da4807d0b8add0ad3838e915e6
Andrew Williams <1williamsaj@gmail.com>
2019-07-20 20:59:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,12 +10,6 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_unshift(array, string)
-  array = ["1", "4"]
-  string = "5"
-  array.unshift(string)
-end
-
 def using_pop(array)
   array = ["milk", "cheese", "bread"]
   array.pop
