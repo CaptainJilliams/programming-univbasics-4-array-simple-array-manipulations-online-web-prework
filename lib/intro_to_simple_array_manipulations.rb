3449aa348ebb560e79cb14739c23c871cfe594db
Andrew Williams <1williamsaj@gmail.com>
2019-07-20 21:42:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,5 +39,5 @@ end
 def using_insert(array, element)
   array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   element = "Python"
-  array.insert(element) 
+  array[4].insert(element) 
 end
